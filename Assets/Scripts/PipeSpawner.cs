e42cd7dda2dede9bb768a0d7604550665a9ae4d8
AliAngel564 <jonathan.aliangel@gmail.com>
2026-02-19 23:11:51 -0600

    💩

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -1,14 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class spawner : MonoBehaviour
 {
     public float queueTime = 1.5f;
     private float time = 0;
     public GameObject obstacle;
+    private Transform spawnPoint;
 
     public float height;
+
+    void Start()
+    {
+        spawnPoint = GetComponent<Transform>();    
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,7 +24,7 @@ public class spawner : MonoBehaviour
         if(time > queueTime)
         {
             GameObject go = Instantiate(obstacle);
-            go.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            go.transform.position = spawnPoint.position + new Vector3(0, Random.Range(-height, height), 0);
 
             time = 0;
 
